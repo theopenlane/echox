1d7f3f5c92daba00cb79bc4d0e4d8331593aa457
Matthew Anderson <42154938+matoszz@users.noreply.github.com>
2025-01-12 11:18:04 -0700

    update license, merge several changes from v4 main

---
+++ renderer_test.go
@@ -0,0 +1,29 @@
+package echox
+
+import (
+	"html/template"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRenderWithTemplateRenderer(t *testing.T) {
+	e := New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(userJSON))
+	rec := httptest.NewRecorder()
+
+	c := e.NewContext(req, rec)
+
+	e.Renderer = &TemplateRenderer{
+		Template: template.Must(template.New("hello").Parse("Hello, {{.}}!")),
+	}
+
+	err := c.Render(http.StatusOK, "hello", "Jon Snow")
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		assert.Equal(t, "Hello, Jon Snow!", rec.Body.String())
+	}
+}
